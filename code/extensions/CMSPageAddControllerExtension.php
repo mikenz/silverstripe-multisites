6abb75573feb627b91d0fd7d45e9a5459af39821
Mike Cochrane <mike.cochrane@airnz.co.nz>
2015-09-02 07:30:29 +1200

    Don't list blacklisted page types when adding pages

--- code/extensions/CMSPageAddControllerExtension.php
+++ code/extensions/CMSPageAddControllerExtension.php
@@ -2,6 +2,25 @@
 class CMSPageAddControllerExtension extends Extension {
 
 	function updatePageOptions(&$fields) {
+		// Any blacklisted page types?
+		$blacklisted = array();
+		$subsite = Subsite::currentSubsite();
+		if($subsite && $subsite->exists() && $subsite->PageTypeBlacklist) {
+			$blacklisted = explode(',', $subsite->PageTypeBlacklist);
+		}
+
+		if ($blacklisted) {
+			// Remove blacklisted page types
+			$pageTypes = $fields->fieldByName('PageType');
+			$source = $pageTypes->getSource();
+			foreach($source as $k => $v) {
+				if (in_array($k, $blacklisted)) {
+					unset($source[$k]);
+				}
+			}
+			$pageTypes->setSource($source);
+		}
+
 		$fields->push(new HiddenField('SubsiteID', 'SubsiteID', Subsite::currentSubsiteID()));
 	}
 
